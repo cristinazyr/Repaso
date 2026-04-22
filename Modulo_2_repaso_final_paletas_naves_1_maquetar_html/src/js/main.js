ba12ad42a95be5763ee1f125d7774cae3c4b8ca3
cristinazyr <criscristina725@hotmail.com>
2026-04-22 13:14:28 +0200

    favoritos

--- Modulo_2_repaso_final_paletas_naves_1_maquetar_html/src/js/main.js
+++ Modulo_2_repaso_final_paletas_naves_1_maquetar_html/src/js/main.js
@@ -2,15 +2,17 @@
 
 //QUERY SELECTOR
 const palettesUl = document.querySelector(".js__paletesUl");
+const favouritesUl = document.querySelector(".js__favouritesUl");
 //DATOS
 let data = [];
+let favourites = [];
 //FUNCIONES
 function createLiforStarShip(oneObject) {
   let colorsHTML = "";
   for (let i = 0; i < oneObject.colors.length; i++) {
     colorsHTML += `<li class="color" style="background-color:#${oneObject.colors[i]}"></li>`;
   }
-  const html = ` <li class="paletteCard">
+  const html = ` <li class="js__paletteCard paletteCard" data-id=${oneObject.id}>
             <h3>${oneObject.name}</h3>
             <ul class="colors">
 ${colorsHTML}
@@ -30,9 +32,42 @@ function renderStarShips() {
     html += createLiforStarShip(oneObject);
   }
   palettesUl.innerHTML = html;
+  const paletteAllCards = document.querySelectorAll(".js__paletteCard");
+  for (const eachCardLi of paletteAllCards) {
+    eachCardLi.addEventListener("click", handdleClickCard);
+  }
 }
-
+function renderFavourites() {
+  let html = "";
+  for (const oneObject of favourites) {
+    html += createLiforStarShip(oneObject);
+  }
+  favouritesUl.innerHTML = html;
+}
 //FUNCIONES DE EVENTOS
+function handdleClickCard(ev) {
+  const clickedPaletteId = ev.currentTarget.dataset.id;
+  // Busca un obj en data que tenga el id=clickedPaletteId
+  const clickedPaletteObj = data.find(
+    (eachPaletteObj) => eachPaletteObj.id === clickedPaletteId,
+  );
+  //Busca un obj en favoritos que el id=clickedPaletteId me devuelve su posicion(index)
+  const clickedFavouriteIndex = favourites.findIndex(
+    (eachPaletteObj) => eachPaletteObj.id === clickedPaletteId,
+  );
+  if (clickedFavouriteIndex === -1) {
+    favourites.push(clickedPaletteObj);
+  }
+  console.log(clickedPaletteObj);
+
+  renderFavourites();
+  /*  const clickedPaletteObj = data.find(
+    (eachPaletteObj) => eachPaletteObj.id === clickedPaletteId,
+  );
+  const html = createLiforStarShip(clickedPaletteObj);
+  favouritesUl.innerHTML += html;
+  ev.currentTarget.classList.toggle("favourite"); */
+}
 
 //EVENTOS
 
